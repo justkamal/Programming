eb1e6f7c181f37d8db13af4e0a3048a3d5f70830
justkamal <kv18995@gmail.com>
2023-02-15 22:53:16 +0530

    Interview practice update

--- TakeUForward/DynamicProgramming/MinSumPath.java
+++ TakeUForward/DynamicProgramming/MinSumPath.java
@@ -3,7 +3,6 @@ package DynamicProgramming;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class MinSumPath {
@@ -13,15 +12,15 @@ public class MinSumPath {
 
     public static void main(String[] args) throws IOException {
         int t = Integer.parseInt(br.readLine());
-        while(t-- > 0){
+        while (t-- > 0) {
             int N = Integer.parseInt(br.readLine());
             int n = N;
             int[][] arr = new int[n][n];
-            while(n > 0){
+            while (n > 0) {
                 strz = new StringTokenizer(br.readLine());
                 int i = 0;
-                while (strz.hasMoreTokens()){
-                   arr[N-n][i++] =  Integer.parseInt(strz.nextToken());
+                while (strz.hasMoreTokens()) {
+                    arr[N - n][i++] = Integer.parseInt(strz.nextToken());
                 }
                 --n;
             }
@@ -31,29 +30,22 @@ public class MinSumPath {
 
     public static int minimumPathSum(int[][] triangle, int n) {
         int up, leftDiag;
-        for(int i = 0; i < n; ++i) {
-            for(int j = 0; j < n - i + 1; ++j) {
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j <= i; ++j) {
                 up = leftDiag = Integer.MAX_VALUE;
-                if(i == 0 && j == 0){
-                    continue;
-                }else {
-                    if(i > 0 && n-i >= j){
-                        up = Math.min(up, triangle[i-1][j]);
-                    }
-                    if(i > 0 && j > 0){
-                        leftDiag = Math.min(leftDiag, triangle[i-1][j-1]);
-                    }
-                    triangle[i][j] += Math.min(leftDiag, up);
+                if (j <= i - 1) {
+                    up = Math.min(up, triangle[i - 1][j]);
                 }
-            }
-            for(int k = 0; k < n; ++k){
-                System.out.println(Arrays.toString(triangle[k]));
+                if (j > 0) {
+                    leftDiag = Math.min(leftDiag, triangle[i - 1][j - 1]);
+                }
+                triangle[i][j] += Math.min(leftDiag, up);
             }
         }
 
         int min = Integer.MAX_VALUE;
-        for(int i = 0; i < triangle[n-1].length; ++i){
-            min = Math.min(min, triangle[n-1][i]);
+        for (int i = 0; i < triangle[n - 1].length; ++i) {
+            min = Math.min(min, triangle[n - 1][i]);
         }
 
         return min;
